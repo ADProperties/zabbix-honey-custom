1c6df94fd3a641e821b505119f37bc6d633c75c4
David Dias <david.dias@glinttglobal.com>
2026-05-05 19:02:26 +0100

    Apagar olho

--- actions/GetTickets.php
+++ actions/GetTickets.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types = 0);
+
 namespace Modules\HoneyCustom\Actions;
 
 use CController;
-use CControllerResponseData;
 
 class GetTickets extends CController {
 
@@ -16,22 +16,6 @@ class GetTickets extends CController {
     protected function doAction(): void {
         $file = __DIR__ . '/../tickets.json';
         $tickets = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
-        
-        $input = json_decode(file_get_contents('php://input'), true);
-        $zeroHosts = $input['zero_hosts'] ?? [];
-        
-        // Se houver anomalias a zeros, apaga do ficheiro (Fechou o Ticket)
-        $changed = false;
-        foreach ($zeroHosts as $zh) {
-            if (isset($tickets[$zh])) {
-                unset($tickets[$zh]);
-                $changed = true;
-            }
-        }
-        
-        if ($changed) {
-            file_put_contents($file, json_encode($tickets));
-        }
 
         header('Content-Type: application/json');
         echo json_encode($tickets);
